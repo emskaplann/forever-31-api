b2aeb28c3ec05e47ce3696a6726a467132fdac9a
Emirhan Kaplan <emskaplann@gmail.com>
2019-12-23 19:07:25 -0500

    uniqeness bug fixed

--- app/models/products_user.rb
+++ app/models/products_user.rb
@@ -6,10 +6,10 @@ class ProductsUser < ApplicationRecord
   # checking if cart is already created
   private
   def uniq_product_and_user
-      @all_couples = ProductsUser.all.map{ |pu| pu.user_id + pu.product_id }
+      @all_couples = ProductsUser.all.map{ |pu| pu.user_id.to_s + pu.product_id.to_s }
       if @all_couples.include?(self.user_id + self.product_id)
         errors.add(:product_id, "should be unique")
       end
   end
-  
+
 end
--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -4,10 +4,12 @@ class WishList < ApplicationRecord
   validate :uniq_product_and_user, on: :create
 
   # checking if wishlist is already created
+  private
   def uniq_product_and_user
-      @all_couples = WishList.all.map{ |pu| pu.user_id + pu.product_id }
+      @all_couples = WishList.all.map{ |pu| pu.user_id.to_s + pu.product_id.to_s }
       if @all_couples.include?(self.user_id + self.product_id)
         errors.add(:product_id, "should be unique")
       end
   end
+
 end
