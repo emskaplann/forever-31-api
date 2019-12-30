9f9499e163b6aa504cb8c2f88e1a444fdd12ac1b
Emirhan Kaplan <emskaplann@gmail.com>
2019-12-30 16:03:33 -0500

    savepoint

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :brand, :display_name, :category_name, :on_sale, :description, :list_price, :image_extension, :images, :variant_images
+  attributes :id, :brand, :display_name, :category_name, :on_sale, :description, :list_price, :image_extension, :shipping_fee, :images, :variant_images
 
   def images
     self.object.product_images
